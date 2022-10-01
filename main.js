9febf690eb339bea52de4739bdd2572adb95a922
unknown <prodipcv1@gmail.com>
2022-10-01 06:28:57 +0600

    timer update

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-$(document).ready(function() {
-  $('.flipTimer').flipTimer({ 
-  
-  // count up or countdown
-  direction: 'down', 
-  
-  // the target date
-  date: 'October 08, 2022 09:30:00', 
-  
-  // callback works only with direction = "down"
-  callback: function() { alert('times up!'); }
-  
-  
+// $(document).ready(function() {
+//   $('.flipTimer').flipTimer({
+
+//   // count up or countdown
+//   direction: 'down',
+
+//   // the target date
+//   date: 'October 08, 2022 09:30:00',
+
+//   // callback works only with direction = "down"
+//   callback: function() { alert('times up!'); }
+
+//   });
+//   });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let timer_ = 1665199800; //unix timestamp
+  let flipdown = new FlipDown(timer_).start().ifEnded(() => {
+    document.querySelector(".flipdown").innerHTML = `<h2>Timer is ended</h2>`;
   });
-  });
+});
